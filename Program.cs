8ef4b04011a9cae4383d9f9ffa9e8a17c1345e19
LucaSk-x <lucapiccinno64@gmail.com>
2022-11-03 15:30:46 +0100

    mid commit

--- Program.cs
+++ Program.cs
@@ -2,6 +2,27 @@
 Console.WriteLine("Hello, World!");
 
 
+Random rd = new Random();
+
+Macchina Lavatrice1 = new Macchina(rd.Next(5, 30), true, rd.Next(20), rd.Next(500), rd.Next(1000));
+Macchina Lavatrice2 = new Macchina(rd.Next(5, 30), false, rd.Next(20), rd.Next(500), rd.Next(1000));
+Macchina Lavatrice3 = new Macchina(rd.Next(5, 30), false, rd.Next(20), rd.Next(500), rd.Next(1000));
+Macchina Lavatrice4 = new Macchina(rd.Next(5, 30), false, rd.Next(20), rd.Next(500), rd.Next(1000));
+Macchina Lavatrice5 = new Macchina(rd.Next(5, 30), true, rd.Next(20), rd.Next(500), rd.Next(1000));
+
+Macchina Asciugatrice1 = new Macchina(rd.Next(5, 30), true, rd.Next(20));
+Macchina Asciugatrice2 = new Macchina(rd.Next(5, 30), false, rd.Next(20));
+Macchina Asciugatrice3 = new Macchina(rd.Next(5, 30), true, rd.Next(20));
+Macchina Asciugatrice4 = new Macchina(rd.Next(5, 30), false, rd.Next(20));
+Macchina Asciugatrice5 = new Macchina(rd.Next(5, 30), true, rd.Next(20));
+
+Console.WriteLine(Lavatrice5.Ammorbidente);
+Console.WriteLine(Lavatrice5.Gettoni);
+Console.WriteLine(Lavatrice1.Ammorbidente);
+Console.WriteLine(Asciugatrice5.Gettoni);
+Console.WriteLine(Asciugatrice3.Gettoni);
+
+
 public class Macchina
 {
     private int gettoni;
@@ -11,12 +32,30 @@ public class Macchina
     private int detersivo;
 
 
+    //Lavatrice
+    public Macchina(int gettoni, bool inUso, int tempo, int ammorbidente, int detersivo)
+    {
+        this.Gettoni = gettoni;
+        this.InUso = inUso;
+        this.Tempo = tempo;
+        this.Ammorbidente = ammorbidente;
+        this.Detersivo = detersivo;
+    }
+
+    //Asciugatrice
+    public Macchina(int gettoni, bool inUso, int tempo)
+    {
+        this.Gettoni = gettoni;
+        this.InUso = inUso;
+        this.Tempo = tempo;
+    }
+
 
     //Gettoni
     public int Gettoni { get; set; }
 
     //InUso
-    public int InUso { get; set; }
+    public bool InUso { get; set; }
 
     //Tempo
     public int Tempo { get; set; }
@@ -26,4 +65,15 @@ public class Macchina
 
     //Detersivo
     public int Detersivo { get; set; }
+
+    public string stampaStato()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (i < 5)
+            {
+                if (Lavatrice1.InUso == true)
+            }
+        }
+    }
 }
