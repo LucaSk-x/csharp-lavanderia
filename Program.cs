0c64831d1afacc2c36ef17c66c5ab8c19fedd718
LucaSk-x <lucapiccinno64@gmail.com>
2022-11-03 14:07:56 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class Macchina
+{
+    private int gettoni;
+    private bool inUso;
+    private int tempo;
+    private int ammorbidente;
+    private int detersivo;
+
+
+
+    //Gettoni
+    public int Gettoni { get; set; }
+
+    //InUso
+    public int InUso { get; set; }
+
+    //Tempo
+    public int Tempo { get; set; }
+
+    //Ammorbidente
+    public int Ammorbidente { get; set; }
+
+    //Detersivo
+    public int Detersivo { get; set; }
+}
